3dcc745359f1f239ec671a93a3534ade7a00d154
dan <phuhuong646@gmail.com>
2024-06-17 22:25:46 +0700

    spring initializr

---
+++ src/main/java/com/dan_michael/example/demo/PjCdtnApplication.java
@@ -0,0 +1,13 @@
+package com.dan_michael.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PjCdtnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PjCdtnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dan_michael/example/demo/PjCdtnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dan_michael.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PjCdtnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
